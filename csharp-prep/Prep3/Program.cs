44bd59f617efa75970f382e523f4afc285f824a6
bentonah <han21002@byui.edu>
2023-11-04 22:24:31 -0600

    W01 Prove Assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        string goAgain;
+
+        do
+        {
+            int number = random.Next(1, 101);
+            int guesses = 0;
+            int guess;
+
+            Console.WriteLine("Welcome to the Magic Number Guessing Game!");
+            Console.WriteLine("Try and guess the number between 1 and 100! ");
+
+            do
+            {
+                Console.Write("What is your guess?  ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher than that.");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower than that.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You guessed the number in {guesses} guesses.");
+                }
+
+            } while (guess != number);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            goAgain = Console.ReadLine();
+
+        } while (goAgain.ToLower() == "yes");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,90 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number, or enter 0 to quit: ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = CalculateSum(numbers);
+        float average = CalculateAverage(numbers);
+        int max = FindMax(numbers);
+        int smallestPositive = FindSmallestPositive(numbers);
+        List<int> sortedNumbers = SortNumbers(numbers);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine($"The smallest positive number closest to zero is: {smallestPositive}");
+
+        Console.WriteLine("Sorted Numbers:");
+        foreach (int number in sortedNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    static int CalculateSum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    static float CalculateAverage(List<int> numbers)
+    {
+        int sum = CalculateSum(numbers);
+        float average = ((float)sum) / numbers.Count;
+        return average;
+    }
+
+    static int FindMax(List<int> numbers)
+    {
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    static int FindSmallestPositive(List<int> numbers)
+    {
+        int smallestPositive = int.MaxValue;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+        return smallestPositive;
+    }
+    
+    static List<int> SortNumbers(List<int> numbers)
+    {
+        List<int> sortedNumbers = new List<int>(numbers);
+        sortedNumbers.Sort();
+        return sortedNumbers;
     }
 }
